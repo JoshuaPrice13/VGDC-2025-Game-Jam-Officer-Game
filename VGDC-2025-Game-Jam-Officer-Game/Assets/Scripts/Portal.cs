db2ff747d60d66922b725d5e45cd5dfb61e15a12
Joshua Price <78606903+JoshuaPrice13@users.noreply.github.com>
2025-11-17 18:21:58 -0600

    Added Portal and platforms

--- VGDC-2025-Game-Jam-Officer-Game/Assets/Scripts/Portal.cs
+++ VGDC-2025-Game-Jam-Officer-Game/Assets/Scripts/Portal.cs
@@ -2,9 +2,25 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    [SerializeField] private Animator PortalAnimator;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        SceneChanger sceneChanger = FindObjectOfType<SceneChanger>();
-        sceneChanger.LoadNextScene();
+        Debug.Log("hit portal");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //PortalAnimator.SetBool("TouchedPortal", true);
+            SceneChanger sceneChanger = FindObjectOfType<SceneChanger>();
+
+            if (sceneChanger != null)
+            {
+                sceneChanger.LoadNextScene();
+            }
+            else
+            {
+                Debug.LogError("SceneChanger not found in scene!");
+            }
+        }
+
     }
 }
