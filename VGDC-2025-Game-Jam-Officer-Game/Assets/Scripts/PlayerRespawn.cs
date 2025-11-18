1f74951ba53b7665231ae972fa60134cf17f1e10
Myanglioce <myanglioce@gmail.com>
2025-11-17 20:24:02 -0600

    I did stuff; ask me about it

---
+++ VGDC-2025-Game-Jam-Officer-Game/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y < -20)
+        {
+            Destroy(this);
+        }
+
+        if (this == null)
+        {
+
+        }
+    }
+}
